08c104b2005330116dd269a00f02198a80a426ed
Anusha Malempati <anumalempati99@gmail.com>
2024-08-28 11:31:11 -0600

    Created resume project with home, resume and conatct pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route should point to HomeComponent
+  { path: 'resume', component: ResumeComponent },
+  { path: 'contact', component: ContactComponent },
+];
---
+++ src/app/resume/resume.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-resume',
+  standalone: true,
+  imports: [],
+  templateUrl: './resume.component.html',
+  styleUrl: './resume.component.css'
+})
+export class ResumeComponent {
+  constructor(private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
+
+}
